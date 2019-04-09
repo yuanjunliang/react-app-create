f6b38ee4912d94fc0cad53903f6f2c6902b87030
yuanjunliang <1610712512@qq.com>
2019-04-09 22:52:17 +0800

    update package.json

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -27,7 +27,16 @@ module.exports = {
                     }
                 }
             ]},
-            {test:/\.js$/,use:"babel-loader"}
+            {
+                test:/\.js$/,
+                exclude:/(node_modules|bower_components)/,
+                use:{
+                    loader: 'babel-loader',
+                    options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
         ]
     },
     plugins:[
